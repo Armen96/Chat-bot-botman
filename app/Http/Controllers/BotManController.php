9557a0af78439799a0685e71c685a55acf6035bf
Armen Barsegyan <barsegyan96armen@gmail.com>
2018-12-22 00:18:27 +0400

    controller fix

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -36,4 +36,9 @@ class BotManController extends Controller
     {
         $bot->startConversation(new ExampleConversation());
     }
+
+    public function welcome()
+    {
+        return view('welcome');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'BotManController@welcome');
 
 Route::match(['get', 'post'], '/botman', 'BotManController@handle');
 Route::get('/botman/tinker', 'BotManController@tinker');
