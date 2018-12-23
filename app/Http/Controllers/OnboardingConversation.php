b0e659e60ebcfe4a982458eb352869ad441ae5fe
Armen Barsegyan <barsegyan96armen@gmail.com>
2018-12-23 21:43:02 +0400

    mail configs 2

--- app/Http/Controllers/OnboardingConversation.php
+++ app/Http/Controllers/OnboardingConversation.php
@@ -66,7 +66,7 @@ class OnboardingConversation extends Conversation
             $mail->Username = getenv('MAIL_USERNAME');                 // SMTP username
             $mail->Password = getenv('MAIL_USERNAME');                           // SMTP password
             $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-            $mail->Port = 587;                                    // TCP port to connect to
+            $mail->Port = 587;
 
             //Recipients
             $mail->setFrom('barsegyan96armen@gmail.com', 'Mailer');
@@ -78,7 +78,19 @@ class OnboardingConversation extends Conversation
 
             //Attachments
 //            $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
+            //Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+        }
 
     }
 
